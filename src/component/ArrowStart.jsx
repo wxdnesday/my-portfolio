9fa7f1fff7bd9983610f97177197f389271b2cc8
wxdnesday <todayis.wednesday111@gmail.com>
2023-06-06 16:17:31 -0700

    Loading Screen Draft

---
+++ src/component/ArrowStart.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { motion } from "framer-motion";
+
+
+export function ArrowStart() {
+    const parent = {
+        variantA: { scale: 1 },
+        variantB: { scale: 1.25 }
+    };
+
+    const child = {
+        variantA: { opacity: 0, left: 0 },
+        variantB: { opacity: 1, left: 1 }
+    };
+
+    return (
+        <motion.div
+            style={{
+                width: 300,
+                height: 85,
+                borderRadius: 30,
+                backgroundColor: "rgba(255,255,255,0)",
+                position: "relative"
+            }}
+            variants={parent}
+            initial="variantA"
+            whileHover="variantB"
+        >
+            <div className = "buttonSelect">
+                START
+            </div>
+            <motion.div
+                style={{
+                    width: 85,
+                    height: 85,
+                    borderRadius: "20px 20px 30px 20px",
+                    backgroundColor: "#fff",
+                    position: "relative",
+                    bottom: 0,
+                    right: 0
+                }}
+                variants={child}
+                transition={{
+                    type: "spring",
+                    stiffness: 120,
+                    duration: 2,
+                    delayChildren: 0.2
+                }}
+            />
+        </motion.div>
+    );
+}
+
+export default ArrowStart
--- src/component/Start.jsx
+++ src/component/Start.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import videoBg from '../assets/vloop.mp4'
+import React from 'react';
+import videoBg from '../assets/vloop.mp4';
 import { motion } from "framer-motion";
+import { ArrowStart } from './ArrowStart';
+import { ArrowOptions } from './ArrowOptions';
+
+
+
 const Start = () => {
   return (
     <div className='start'>
         <div className="overlay"></div>
         <video src={videoBg} muted loop autoPlay />
         <div className="content">
+          <div className="welcome-title">
             <h1>Welcome</h1>
-            <motion.button
-              whileHover={{ scale: 1.1 }}
-              className="start-button"
-              onClick={() => null}
-            >
-              START
-            </motion.button>
+          </div>
+          <div className="arrow-buttons">
+            <ArrowStart />
+            <ArrowOptions />
+          </div>
         </div>
 
     </div>
